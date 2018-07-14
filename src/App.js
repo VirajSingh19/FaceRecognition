4a9ac65d0621a83e7a721cd15273be2f556e8c36
Viraj Singh <virajsingh19@yahoo.com>
2018-07-15 02:18:05 +0530

    added particle react component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,34 @@ import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo'
 import ImageLink from './Components/ImageLink/ImageLink'
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+
+const praticleoptions = {
+  particles: {
+    number :{
+      value:190,
+      density:{
+        enable:true,
+        value_area:800
+      }
+    }
+    
+  }
+}
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+       <Particles className="p"
+              params={  praticleoptions 	}
+            />
         <Navigation/>
         <Logo/>
+        <Rank/>
         <ImageLink/>
         {/*<FaceRecognition/> */}
       </div>
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,15 @@
+import React from 'react';
+const Rank = () => {
+    return(
+      <div>
+            <div>
+                {'Andrei your current rank is...'}
+            </div>
+            <div className='purple f3'>
+                {'#5'}
+            </div>
+      </div>
+      );
+}
+
+export default Rank;
